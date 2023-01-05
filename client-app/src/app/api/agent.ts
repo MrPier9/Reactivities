06014e6ac6c61aa7d26be7b9be617571f659783b
Pierluigi Secco <pierluigi.secco@edu.unito.it>
2023-01-05 01:51:00 +0100

    end of section 6

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../modules/activities";
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    });
+}
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(800);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody)
+}
+
+const Activities = {
+    list: () => requests.get('/activities'),
+    details: (id: string) => requests.get(`/activities/${id}`),
+    create: (activity: Activity) => axios.post('/activities', activity),
+    update: (activity: Activity) => axios.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del(`/activities/${id}`)
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,4 +1,5 @@
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Activity } from "../../../app/modules/activities";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -14,20 +15,25 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    loading: boolean;
+    submitting: boolean
 }
 
-export default function ActivityDashboard({ activities, selectActivity, selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectActivity, selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, loading, submitting }: Props) {
+
+    if (loading) return <LoadingComponent />
+
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} />
             </Grid.Column>
             <Grid.Column width={6}>
                 {selectedActivity && !editMode &&
                     <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />
                 }
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting} />
                 }
             </Grid.Column>
         </Grid>
