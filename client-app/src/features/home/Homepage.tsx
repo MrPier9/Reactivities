0d56e8eb98d829b8f244d2fde997153ba59dc87f
Pierluigi Secco <pierluigi.secco@edu.unito.it>
2023-01-09 15:37:42 +0100

    end of section 9

--- client-app/src/features/home/Homepage.tsx
+++ client-app/src/features/home/Homepage.tsx
@@ -10,7 +10,7 @@ export default function Homepage() {
                     Reactivities
                 </Header>
                 <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/activities' size="huge" inverted >Take to the activities</Button>
+                <Button as={Link} to='/activities' size="huge" inverted >Take me to the activities</Button>
             </Container>
         </Segment>
     )
