d51ea65feb290ed14eb2e19d7ab8c1bdda729200
Pierluigi Secco <pierluigi.secco@edu.unito.it>
2023-01-11 10:30:39 +0100

    end of section 12

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage =
+            "Password must contain at least 1 capital, 1 digit, 1 special character and must be between 6-10 characters")]
+        public string Password { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        public string Username { get; set; }
+    }
+}
