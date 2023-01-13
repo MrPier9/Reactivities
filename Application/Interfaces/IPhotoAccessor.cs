b2425a14c648756d1e0164ad08f0ec56f440679d
Pierluigi Secco <pierluigi.secco@edu.unito.it>
2023-01-13 02:16:56 +0100

    end of section 16

---
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -0,0 +1,12 @@
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Interfaces
+{
+    public interface IPhotoAccessor
+    {
+        Task<PhotoUploadResult> AddPhoto(IFormFile file);
+        Task<string> DeletePhoto(string publicId);
+    }
+}
